7ac5bbd80548fb4e4a443bf9be8755a61eb525df
Francis Lavazelli <francis.lavazelli@gmail.com>
2025-04-20 19:09:37 -0400

    IaC files for infra

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,3 +1,11 @@
-output "instance_ip" {
-  value = google_compute_instance.mongodb.network_interface[0].access_config[0].nat_ip
+output "mongodb_internal_ip" {
+  value = google_compute_instance.mongodb_vm.network_interface[0].network_ip
+}
+
+# output "cloud_run_url" {
+#   value = google_cloud_run_service.nextjs_app.status[0].url
+# }
+
+output "backup_bucket_url" {
+  value = google_storage_bucket.mongo_backups.url
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,23 +1,25 @@
-variable "project_id" {
-  description = "GCP project ID"
-  type        = string
+variable "project_id" {}
+variable "region" { default = "us-central1" }
+variable "zone" { default = "us-central1-a" }
+
+variable "vpc_connector_range" {
+  default = "10.8.0.0/28"
 }
 
-variable "region" {
-  default = "us-central1"
-}
-
-variable "zone" {
-  default = "us-central1-a"
-}
-
-variable "machine_type" {
-  default = "e2-micro"
-}
-
-variable "admin_password" {
-  description = "MongoDB admin password"
-  type        = string
+variable "mongo_password" {
+  description = "MongoDB root password"
   sensitive   = true
 }
 
+variable "mongo_db" {
+  default = "mydb"
+}
+
+variable "backup_bucket" {
+  default = "your-mongo-backups"
+}
+
+variable "grafana_password" {
+  default = "password"
+  sensitive = true
+}
