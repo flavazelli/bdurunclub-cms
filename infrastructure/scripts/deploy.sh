ba17a8d3d8eb01f006395e8ab44981407c03f306
Francis Lavazelli <francis.lavazelli@gmail.com>
2025-04-27 23:58:50 -0400

    modifying deploy script

--- infrastructure/scripts/deploy.sh
+++ infrastructure/scripts/deploy.sh
@@ -1,7 +1,7 @@
 gcloud secrets versions access latest --secret=\"app_env\" > app_env.json
 # Convert the fetched secret JSON to environment variable format
-cat app_env.json | jq -r 'to_entries[] | "\(.key)=\(.value)"' > /opt/infra/docker-compose/.env
+cat app_env.json | jq -r "to_entries[] | \"\(.key)=\(.value)\"" > /opt/infra/docker-compose/.env
 rm app_env.json
 cd /opt/infra/docker-compose
 sudo docker-compose pull backend
-sudo docker-compose restart backend
+sudo docker-compose up -d backend
